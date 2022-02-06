028e8e1e0c232894efb9bd7ec5a2f2de105c69f1
trungn <tknghiem1@gmail.com>
2022-02-05 19:37:47 -0800

    add rainbow functionality

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function createGrid() {
     let size = 16;
 
     if(toggle) {
-        size = +prompt('Enter number N from 1 to 100 for N x N Grid');
+        size = +prompt('Enter number N from 1 to 100 for N x N Grid.');
         if(!(size >=1 && size <= 100)) alert('Error. Click "Create New Grid" to try again.');
     }
 
@@ -32,15 +32,19 @@ function createGrid() {
 }
 
 function changeColor(e) {
-    if(e.target.className = 'square') {
-        e.target.style.backgroundColor = 'black';
 
+    if(e.target.className = 'square') {
+        const colorNum1 = Math.floor(Math.random() * 256);
+        const colorNum2 = Math.floor(Math.random() * 256);
+        const colorNum3 = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${colorNum1},${colorNum2},${colorNum3})`;
     }
+
 }
 
-const createBtn = document.querySelector('.create');
 const main = document.querySelector('.main');
-console.log(main);
+const createBtn = document.querySelector('.create');
+const resetBtn = document.querySelector('.reset');
 
 let toggle = false;
 
